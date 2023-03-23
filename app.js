95309c43cdcd7cf762f46dab4e14d825efaa12e8
Dennis2300 <124138161+Dennis2300@users.noreply.github.com>
2023-03-23 11:51:40 +0100

    update

--- app.js
+++ app.js
@@ -1,71 +1,66 @@
 "use strict";
 window.addEventListener("load", initApp)
 
-const zacian = {
-    description: 
-    "Known as a legendary hero, this Pokémon absorbs metal particles, transforming them into a weapon it uses to battle. Now armed with a weapon it used in ancient times, this Pokémon needs only a single strike to fell even Gigantamax Pokémon",
-    name: "Zacian",
-    ability: "Intrepid Sword",
-    footprint: "Unknown",
-    dexIndex: "0888",
-    type: "Fairy",
-    subtype:"Steel",
-    weakness: "Fire",
-    weakness2:"Ground",
-    gender: "Unknown",
-    weight: "110.0",
-    height: "2.8",
-    generation: "Generation VIII",
-    spilVersion:"Pokemon Sword",
-    canEvolve:"false",
-    health: "6",
-    attack: "8",
-    defense: "7",
-    specialAttack: "7",
-    specialDefense:"9",
-    speed:"5",
+async function initApp() {
+    console.log("JS is running!");
+    const zacian = await getPokemon("https://raw.githubusercontent.com/Dennis2300/Zacian-pokemon-data/main/zacian.json")
+    showPokemon(zacian)
+    document.querySelector("#open_button").addEventListener("click", openDetailView)
 }
 
-function initApp() {
-    showPokemon(zacian)
+async function getPokemon(url) {
+    const response = await fetch(url)
+    const data = response.json()
+    return data;
+}
+
+function openDetailView() {
+    console.log("open");
+    document.querySelector("#open_button").removeEventListener("click",openDetailView)
+    document.querySelector("#detailedView").showModal()
+    document.querySelector("#close_button").addEventListener("click",closeDetailView)
+}
+function closeDetailView() {
+    console.log("close");
+    document.querySelector("#detailedView").close()
 }
 
 function showPokemon(pokemon) {
-console.log("Pokemon: Zacian");
-const pokemonZacian = /*html*/ `
-<article>
-<h1>Zacian</h1>
-<image src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/888_f2.png"></image><br>
-<div id="parent">
-    <p>${pokemon.description}</p>
-    <li>Pokemon Name: ${pokemon.name}</li>
-    <li>Pokedex Nr: #${pokemon.dexIndex}</li>
-    <li>Footprint: ${pokemon.footprint}</li>
-    <li>Ability: ${pokemon.ability}</li>
-    <li>Type: ${pokemon.type}</li>
-    <li>Subtype ${pokemon.subtype}</li>
-    <li>Weakness: ${pokemon.weakness} & ${pokemon.weakness2}</li>
-    <li>Gender: ${pokemon.gender}</li>
-    <li>Weight: ${pokemon.weight} kg</li>
-    <li>Height: ${pokemon.height} meter</li>
-    <li>Gender: ${pokemon.gender}</li>
-    <li>Generation: ${pokemon.generation}</li>
-    <li>Game Version: ${pokemon.spilVersion}</li>
-    <li>Evolve: ${pokemon.canEvolve}</li>
+    document.querySelector("#pokemons").insertAdjacentHTML("beforeend",
+    /*html*/`
+<h1>${pokemon.name}</h1>
+    <div id="picture">
+        <image src="${pokemon.image}"></image>
     </div>
-</article>
-`
-const pokemonZacianStats =/*html*/`
+<p>${pokemon.description}</p>
 <article>
-<h2>Pokemon Stats</h2>
+    <div id="open_button">
+        <button>Detailed View</button>
+    </div>
+<dialog id="detailedView">
+<li>Ability: ${pokemon.ability}</li>
+<li>Footprint: ${pokemon.footprint}</li>
+<li>DexIndex: #${pokemon.dexIndex}</li>
+<li>Type: ${pokemon.type}</li>
+<li>Subtype: ${pokemon.subtype}</li>
+<li>Weakness: ${pokemon.weakness} & ${pokemon.weakness2}</li>
+<li>Gender: ${pokemon.gender}</li>
+<li>Weight: ${pokemon.weight} kg</li>
+<li>Height: ${pokemon.height} meter</li>
+<li>Generation: ${pokemon.generation}</li>
+<li>Game Version: ${pokemon.spilVersion}</li>
+<li>Can it evolve?: ${pokemon.canEvolve}</li>
 <li>Health: ${pokemon.health}</li>
-<li>Speed: ${pokemon.speed}</li>
 <li>Attack: ${pokemon.attack}</li>
 <li>Defense: ${pokemon.defense}</li>
 <li>Special Attack: ${pokemon.specialAttack}</li>
-<li>Special Defense: ${pokemon.specialDefense}</li>
+<li>SPecial Defense: ${pokemon.specialDefense}</li>
+<li>Speed: ${pokemon.speed}</li>
+<br>
+    <div id="close_button">
+        <button>Close</button>
+    </div>
+</dialog>
 </article>
-`
-document.querySelector("#pokemons").insertAdjacentHTML("beforeend", pokemonZacian);
-document.querySelector("#pokemons").insertAdjacentHTML("beforeend", pokemonZacianStats);
+`)
 }
