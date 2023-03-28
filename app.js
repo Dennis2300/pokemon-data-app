e7d11023ebd25fabe168f43675005b2cff1947bc
Dennis2300 <124138161+Dennis2300@users.noreply.github.com>
2023-03-28 11:24:45 +0200

    update

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ async function initApp() {
     console.log("JS is running!");
 const pokemons = await getPokemon();
 
-    pokemons.forEach(showPokemon)
+    //pokemons.forEach(showPokemon)
+    showPokemon(pokemons)
 }
 
 
 async function getPokemon() {
-    const response = await fetch("https://cederdorff.github.io/dat-js/05-data/pokemons.json");
+    const response = await fetch("https://raw.githubusercontent.com/Dennis2300/Zacian-pokemon-data/main/zacian.json");
     const data = response.json();
     console.log(data);
     console.log("Data fetched");
@@ -22,34 +23,39 @@ function showPokemon(pokemon) {
         "beforeend",
     /*html*/ `
     <article>
+    
+    <div id="picture2">
+    <image src="${pokemon.image}"></image>
+    </div>
     <h1>${pokemon.name}</h1>
-
+    <div id="pokedex2">#0${pokemon.dexindex}</div>
+    <div id="type2">Type: ${pokemon.type}</div>
+    <dialog id = "pokemon-stats">
+    <h2 id = "dialog-title">${pokemon.name}</h2>
+    <p>${pokemon.description}</p>
         <div id="picture">
             <image src="${pokemon.image}"></image>
         </div>
-
-    <p>${pokemon.description}</p>
-
-    <dialog id = "pokemon-stats">
-        <h2 id = "dialog-title">${pokemon.name}</h2>
-        <li id = "ability">Ability: ${pokemon.ability}</li>
-        <li id = "footprint">Footprint: ${pokemon.footprint}</li>
-        <li id = "dexIndex">DexIndex: #${pokemon.dexIndex}</li>
-        <li id = "type">Type: ${pokemon.type}</li>
-        <li id = "subType">Subtype: ${pokemon.subtype}</li>
-        <li id = "weakness">Weakness: ${pokemon.weakness}</li>
-        <li id = "gender">Gender: ${pokemon.gender}</li>
-        <li id = "weight">Weight: ${pokemon.weight} kg</li>
-        <li id = "height">Height: ${pokemon.height} meter</li>
-        <li id = "generation">Generation: ${pokemon.generation}</li>
-        <li id = "gameVersion">Game Version: ${pokemon.spilVersion}</li>
-        <li id = "evolve">Can it evolve?: ${pokemon.canEvolve}</li>
-        <li id = "health">Health: ${pokemon.health}</li>
-        <li id = "attack">Attack: ${pokemon.attack}</li>
-        <li id = "defense">Defense: ${pokemon.defense}</li>
-        <li id = "specialAttack">Special Attack: ${pokemon.specialAttack}</li>
-        <li id = "specialDefense">Special Defense: ${pokemon.specialDefense}</li>
-        <li id = "speed">Speed: ${pokemon.speed}</li>
+    <div id="list">
+        <li id = "ability">${pokemon.ability}</li>
+        <li id = "footprint">${pokemon.footprint}</li>
+        <li id = "dexindex">${pokemon.dexindex}</li>
+        <li id = "type">${pokemon.type}</li>
+        <li id = "subtype">${pokemon.subtype}</li>
+        <li id = "weaknesses">${pokemon.weaknesses}</li>
+        <li id = "gender">${pokemon.gender}</li>
+        <li id = "weight">${pokemon.weight}</li>
+        <li id = "height">${pokemon.height}</li>
+        <li id = "generation">${pokemon.generation}</li>
+        <li id = "spilversion">${pokemon.spilversion}</li>
+        <li id = "evolve">${pokemon.canEvolve}</li>
+        <li id = "health">${pokemon.statsHP}</li>
+        <li id = "attack">${pokemon.statsAttack}</li>
+        <li id = "defence">${pokemon.statsDefence}</li>
+        <li id = "specialAttack">${pokemon.statsSpecialAttack}</li>
+        <li id = "specialDefence">${pokemon.statsSpecialDefence}</li>
+        <li id = "speed">${pokemon.statsSpeed}</li>
+    </div>
             <form id="close_button" method="dialog">
 		     <button>Close</button>
             </form>
@@ -63,22 +69,22 @@ document.querySelector("#pokemons article:last-child").addEventListener("click",
             document.querySelector("#dialog-title").textContent = `${pokemon.name}`
             document.querySelector("#ability").textContent = `Ability: ${pokemon.ability}`
             document.querySelector("#footprint").textContent = `Footprint: ${pokemon.footprint}`
-            document.querySelector("#dexIndex").textContent = `DexIndex: ${pokemon.dexIndex}`
+            document.querySelector("#dexindex").textContent = `DexIndex: #0${pokemon.dexindex}`
             document.querySelector("#type").textContent = `Type: ${pokemon.type}`
-            document.querySelector("#subType").textContent = `Subtype: ${pokemon.subtype}`
-            document.querySelector("#weakness").textContent = `Weakness: ${pokemon.weakness}`
+            document.querySelector("#subtype").textContent = `Subtype: ${pokemon.subtype}`
+            document.querySelector("#weaknesses").textContent = `Weakness: ${pokemon.weaknesses}`
             document.querySelector("#gender").textContent = `Gender: ${pokemon.gender}`
-            document.querySelector("#weight").textContent = `Weight: ${pokemon.weight}`
-            document.querySelector("#height").textContent = `Height: ${pokemon.height}`
+            document.querySelector("#weight").textContent = `Weight: ${pokemon.weight} gram`
+            document.querySelector("#height").textContent = `Height: ${pokemon.height} meter`
             document.querySelector("#generation").textContent = `Generation ${pokemon.generation}`
-            document.querySelector("#gameVersion").textContent = `Spil: ${pokemon.spilVersion}`
-            document.querySelector("#evolve").textContent = `Can it evolve?: ${pokemon.canEvolve}`
-            document.querySelector("#health").textContent = `Health: ${pokemon.health}`
-            document.querySelector("#attack").textContent = `Attack: ${pokemon.attack}`
-            document.querySelector("#defense").textContent = `Defense: ${pokemon.defense}`
-            document.querySelector("#specialAttack").textContent = `Special Attack: ${pokemon.specialAttack}`
-            document.querySelector("#specialDefense").textContent = `Special Defense: ${pokemon.specialDefense}`
-            document.querySelector("#speed").textContent = `Speed: ${pokemon.speed}`
+            document.querySelector("#spilversion").textContent = `Spil: ${pokemon.spilversion}`
+            document.querySelector("#evolve").textContent = `Evolve: ${pokemon.canEvolve}`
+            document.querySelector("#health").textContent = `Health: ${pokemon.statsHP}`
+            document.querySelector("#attack").textContent = `Attack: ${pokemon.statsAttack}`
+            document.querySelector("#defence").textContent = `Defense: ${pokemon.statsDefence}`
+            document.querySelector("#specialAttack").textContent = `Special Attack: ${pokemon.statsSpecialAttack}`
+            document.querySelector("#specialDefence").textContent = `Special Defense: ${pokemon.statsSpecialDefence}`
+            document.querySelector("#speed").textContent = `Speed: ${pokemon.statsSpeed}`
             document.querySelector("#pokemon-stats").showModal()
         }
 }
